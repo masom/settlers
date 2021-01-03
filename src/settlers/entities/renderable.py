2c167327dcb3072a1fece6608b908c744d40cde1
Martin Samson <martin.samson.0286@gmail.com>
2021-01-03 10:40:24 -0500

    Adding some typing

--- src/settlers/entities/renderable.py
+++ src/settlers/entities/renderable.py
@@ -2,17 +2,17 @@ from settlers.engine.components import Component
 
 
 class Renderable(Component):
-    __slots__ = ('sprite', 'type', 'z')
+    __slots__ = ['sprite', 'type', 'z']
 
     exposed_as = 'renderable'
     exposed_methods = ('reset_sprite')
 
-    def __init__(self, owner, type, z=1):
+    def __init__(self, owner, type: str, z: int = 1) -> None:
         super().__init__(owner)
 
         self.sprite = None
         self.type = type
         self.z = z
 
-    def reset_sprite(self) -> None:
+    def reset_sprite(self):
         self.sprite = None
